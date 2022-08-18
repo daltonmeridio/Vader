fa3bcfd35ed8bf38f8be58af42ec52cb2cb33737
Omar sIR <daltonrido@gmail.com>
2022-08-18 18:27:54 -0500

    funcion coonScan y portScan añadidas

--- scann.py
+++ scann.py
@@ -1,4 +1,5 @@
 import optparse 
+from  socket import *
 
 parser = optparse.OptionParser('usage %prog -H' + '<target host> -p <target port')
 
@@ -15,7 +16,37 @@ if (tgtHost == None) |(tgtPort == None):
     print(parser.usage)
 exit(0)
 
+def connScan(tgtHost, tgtPort):
+    try:
+        connSKT = socket(AF_INET, SOCK_STREAM)
+
+        connSKT.connect((tgtHost, tgtPort))
+
+        print("[+]%d tcp open"%tgtPort)
+
+        connSKT.close()
+    except:
+        print("[-]%d tcp closed"%tgtHost)
 
 
+def portScan(tgtHost, tgtPorts):
+    
+    try:
+        tgtIP = gethostbyename(tgtHost)
+    except:
+        print("[-] Cannot resolve '%s': Unknown host"%tgtHost)
 
+    return
 
+    try:
+        tgtNanme = gethostbyaddr(tgtIP)
+        print("\n[+] Scan Result for: " + tgtName[0])
+    except:
+
+        print("\n Scan Result for: " + tgtIP)
+
+    setdefaulttimeout(1)
+
+    for tgtPort in tgtPorts:
+        print("Scanning port" + tgtPort)
+        connScan(tgtHost, int(tgtPort))
