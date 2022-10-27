ee5180002ca7061d0aa1650107bedee3b83ffd9a
Omar sIR <daltonrido@gmail.com>
2022-10-27 16:40:53 -0500

    Ultimas pulidas

--- vader.py
+++ vader.py
@@ -2,71 +2,71 @@ from multiprocessing import Semaphore
 from socket import *
 from threading import Thread
 import socket
-import sys, signal, time, optparse
+import optparse
 
 screenLock = Semaphore(value=1)
 
 
 def main():
-    parser = optparse.OptionParser('Como usar scann.py -H' + ' <target host> -p <target port')
-    parser.add_option('-H', dest='tgtHost', type='string', help='ingrese el host')
-    parser.add_option('-p', dest='tgtPort', type='string', help='ingrese el puerto')
+    parser = optparse.OptionParser('Uso: vader.py -H' + ' <target ip> -p <target port')
+    parser.add_option('-H', dest='Target_IP', type='string', help='ingrese el host')
+    parser.add_option('-p', dest='Target_Port', type='string', help='ingrese el puerto')
     parser.add_option('-t', dest='threading', action='store_true', help='Ingrese los hilos a usar: -t 10')
 
     (options, args) = parser.parse_args()
 
-    if options.tgtHost is None or options.tgtPort is None:
+    if options.Target_IP is None or options.target_Port is None:
         print(parser.usage)
         exit(0)
 
-    tgt_ports = list(map(int, filter(None, map(lambda p: p.strip(), options.tgtPort.split(",")))))
-    portScan(options.tgtHost, tgt_ports, options.threading)
+    target_ports = list(map(int, filter(None, map(lambda p: p.strip(), options.Target_Port.split(",")))))
+    portScan(options.Target_IP, target_ports, options.threading)
 
 
-def connScan(tgtHost, tgtPorts):
+def connScan(Target_IP, Target_Port):
     connSKT = None
 
     try:
         connSKT = socket(AF_INET, SOCK_STREAM)
-        connSKT.connect((tgtHost, tgtPorts))
+        connSKT.connect((Target_IP, Target_Port))
         connSKT.send('Dalton was here\r\n')
         results = connSKT.recv(100)
         screenLock.acquire()
 
-        print(f"[+]%d tcp open {tgtHost}{tgtPorts}")
+        print(f"[+]%d tcp open {Target_IP}{Target_Port}")
         print("[+]" + str(results))
 
     except Exception:
         screenLock.acquire()
-        print(f"[-]%d tcp closed {tgtHost}")
+        print(f"[-]%d tcp closed {Target_IP}")
     finally:
         screenLock.release()
         if connSKT:
             connSKT.close()
 
 
-def portScan(tgtHost, tgtPorts, threading=False):
+def portScan(Target_IP, Target_Port, threading=False):
     
     try:
-        tgtIP = gethostbyname(tgtHost)
+        t_IP = gethostbyname(Target_IP)
     except:
-        print(f"[-] Cannot resolve : Unknown host {tgtIP}")
+        print(f"[-] Cannot resolve : Unknown host {t_IP}")
         return
 
     try:
-        tgtName = gethostbyaddr(tgtIP)
-        print(f"\n[+] Scan Result for: {tgtName[0]}")
+        T_Name = gethostbyaddr(t_IP)
+        print(f"\n[+] Scan Result for: {T_Name[0]}")
     except:
-        print(f"\n Scan Result for: {tgtIP}")
+        print(f"\n Scan Result for: {t_IP}")
 
     setdefaulttimeout(1)
-    for Port in tgtPorts:
-        print(f"[+] Escaneando el host {tgtHost}:{tgtPorts}")
+    for Port in Target_IP:
+        print(f"[+] Escaneando el host {Target_IP}:{Target_Port}")
         if threading:
-            t = Thread(target=connScan, args=(tgtHost, int(Port)))
+            t = Thread(target=connScan, args=(Target_IP, int(Port)))
             t.start()
         else:
-            connScan(tgtHost, tgtPorts)
+            connScan(Target_IP, Target_Port)
 
 
 def banner():
@@ -82,8 +82,8 @@ def banner():
 
     print('< Happy Hacking ! >')
     print('    \    ^ __ ^    ')
-    print('     \   ( oo ) \_______')
-    print('         ( __ ) \        ) \/\/')
+    print('     \   ( oo ) \________\/\/')
+    print('         ( __ ) \        )  ')
     print('             ||----w || ||  ')
 
     
